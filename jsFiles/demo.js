cfe5d2e1fc1d605daca8732334079ddb712d548e
UJadhav01 <111181029+UJadhav01@users.noreply.github.com>
2022-10-04 13:27:33 +0530

    Created demo.js
    First program execution of java script

---
+++ jsFiles/demo.js
@@ -0,0 +1,11 @@
+console.log("Hello everyone,my name is xyz");
+//for hobbies
+console.log("My hobbies are:\n"+1+"\tGardening\n"+2+"\tCooking\n"+3+"\tInternet Surfing\n");
+//for strengths
+console.info("My strengths are:\n"+1+"\tI am positive thinker and self motivator\n"+2+"\tI'm quick learner\n"+3+"\tI have collaborative nature\n");
+//for weaknesses
+console.warn("I have some weaknesses as well:\n"+1+"\tMy helping nature and being good with everyone,but sometimes this puts me in trouble\n"+2+"\tI am a workaholic type person\n"+3+"\tHave a fear of public speaking\n");
+//for output observation only it makes addition if we insert brackets and concatenation if we remove brackets outside of numbers
+console.log("Result of 30+50:\t"+(30+50));
+//for output of multiplication of given numbers
+console.log("result of multiplication of 49*479:\t"+49*479);
