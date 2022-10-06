f09bc2eda4d6985675f7ae15f441d86012bb7be6
UJadhav01 <111181029+UJadhav01@users.noreply.github.com>
2022-10-06 10:44:18 +0530

    variableAss_01.js

---
+++ jsFiles/variablesAss_01.js
@@ -0,0 +1,18 @@
+var myNumber;   //variable declaration
+console.log("\nValue of variable myNumber without initialization:\t",myNumber);
+myNumber=100;   //variable initialization
+console.log("\nValue of variable myNumber after initialization:\t",myNumber);
+console.log("----------------------------------------------------------------------------------------------------------------------------------------------------");
+
+var myLove="I love only JS";    //variable declaration and initialization
+console.log("\nValue of variable myLove after declaring and initializing at same line:\t",myLove);
+console.log("----------------------------------------------------------------------------------------------------------------------------------------------------");
+
+var temp;   //variable declaration
+console.log("\nValue of variable temp without initialization:\t",temp);
+
+temp=200;   //variable initialization
+console.log("\nValue of variable temp after initialization:\t",temp);
+
+temp="JavaScript";//initialization and updating
+console.log("\nUpdated Value of variable temp:\t",temp+"\n");
