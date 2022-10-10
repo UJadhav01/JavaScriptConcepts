fc02da638077ab0ab6199b41018c448078a9c96e
UJadhav01 <111181029+UJadhav01@users.noreply.github.com>
2022-10-10 13:20:21 +0530

    variableAss01.js

---
+++ jsFiles/01_variablesAss.js
@@ -0,0 +1,52 @@
+var myFullName; // variable declaration
+console.log("\nValue of variable myFullName without initialization:\t",myFullName);
+
+myFullName="first and last name only";
+console.log("\nValue of variable myFullName after initialization:\t",myFullName);
+
+myFullName="Full name";
+console.log("\nUpdated Value of variable myFullName:\t",myFullName);
+console.log("----------------------------------------------------------------------------------------------------------------------------------------------------");
+
+var myLove="I love only JS";    //variable declaration and initialization
+console.log("\nValue of variable myLove after declaring and initializing at same line:\t",myLove);
+console.log("----------------------------------------------------------------------------------------------------------------------------------------------------");
+var firstName,lastName,email,graduation,passoutYear,city,citizenship,phone,address;//variables declaration
+console.log("Form controls used in registration form:");
+firstName="xyz";
+lastName="abc";
+email="xyz@gmail.com";
+graduation="Computer science and engineering";
+passoutYear=2017;
+city="city name";
+citizenship="India";
+phone=5555555555;
+address="residential address";
+
+console.log("\nMy name is:\t"+firstName);
+console.log("\nMy last name is:\t"+lastName);
+console.log("\nMy mail address is:\t"+email);
+console.log("\nI have completed my graduation in::\t"+graduation);
+console.log("\nMy graduation pass out year is:\t"+passoutYear);
+console.log("\nI am from:\t"+city);
+console.log("\nMy citizenship is:\t"+citizenship);
+console.log("\nMy working mobile number is:\t"+phone);
+console.log("\nMy address is:\t"+address);
+console.log("----------------------------------------------------------------------------------------------------------------------------------------------------");
+
+var myNumber;   //variable declaration
+console.log("\nValue of variable myNumber without initialization:\t",myNumber);
+myNumber=100;   //variable initialization
+console.log("\nValue of variable myNumber after initialization:\t",myNumber);
+
+
+console.log("----------------------------------------------------------------------------------------------------------------------------------------------------");
+
+var temp;   //variable declaration
+console.log("\nValue of variable temp without initialization:\t",temp);
+
+temp=200;   //variable initialization
+console.log("\nValue of variable temp after initialization:\t",temp);
+
+temp="JavaScript";//initialization and updating
+console.log("\nUpdated Value of variable temp:\t",temp+"\n");
