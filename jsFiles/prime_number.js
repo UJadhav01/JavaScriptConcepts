eafdf61c2d7d5e93d29e860dadf8170f156d8e2d
UJadhav01 <111181029+UJadhav01@users.noreply.github.com>
2022-10-16 10:36:15 +0530

    prime_number.js

---
+++ jsFiles/prime_number.js
@@ -0,0 +1,12 @@
+let num=10;
+for(let i=2;i<Math.sqrt(num);i++){
+
+    if((num%2)==0){
+        console.log(`---------------------------------------------------`);
+        console.log("not prime",i);
+    }
+        else{
+        console.log(`---------------------------------------------------`);
+        console.log("prime",i);
+    }
+}
