99764e2729dcc6c03e8a76d97dc27f3a0f36d2f5
UJadhav01 <111181029+UJadhav01@users.noreply.github.com>
2022-10-17 16:43:18 +0530

    updated vowels.js

--- jsFiles/vowels_in_alphabet.js
+++ jsFiles/vowels_in_alphabet.js
@@ -6,7 +6,9 @@ var constantCount=0;
 //Checks whether a character is a vowel    
 for (let index = 0; index < str.length; index++) {
 if(str.charAt(index) == 'a' || str.charAt(index) == 'e' || str.charAt(index) == 'i' 
-|| str.charAt(index) == 'o' || str.charAt(index) =='u'){
+|| str.charAt(index) == 'o' || str.charAt(index) =='u' || str.charAt(index) == 'A' 
+|| str.charAt(index) == 'E' || str.charAt(index) == 'I' 
+|| str.charAt(index) == 'O' || str.charAt(index) =='U'){
     console.log("String has ", str.charAt(index)," at index " ,index);
     //Increments the vowel counter    
     vowelsCount=vowelsCount+1;
@@ -21,4 +23,15 @@ console.log(`\nThe given string contains total ${vowelsCount} vowels`);
 console.log(`----------------------------------------------------`);
 console.log(`\nThe given string contains total ${constantCount} constant`);
 
+/*var vowels="aeiou";
+if(charAt(index)==vowels){}
+*var givenString = "I love JavaScript";
+var totalChar = givenString.length;
+for (let index = 0; index <totalChar; index++) {
+    if (index%2==0) {
+        var charAt = givenString.charAt(index);
+        console.log(charAt); 
+    }  
+}
+ */
 
