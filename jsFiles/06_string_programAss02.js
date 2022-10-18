a8655f1f8d38f51645e6c2a994b4a50083d8f246
UJadhav01 <111181029+UJadhav01@users.noreply.github.com>
2022-10-18 09:35:56 +0530

    for_loop _stringAss2.js

--- jsFiles/06_string_programAss02.js
+++ jsFiles/06_string_programAss02.js
@@ -1,6 +1,6 @@
-console.log("\nStep-1 : Program for reverse given string1=Hard work always pays back ,\n string2=Soon I will be Angular IT Champ using only charAt()");
+console.log("\nStep-1 : Program for reverse given string1=Hard work always pays back ,\n\t string2=Soon I will be Angular IT Champ using only charAt()");
 //function for display even position characters
-console.log(`\nGiven string is string1 = "Hard work always pays back" \n\t\tstring2 = Soon I will be Angular IT Champ`);
+//console.log(`\nGiven string is string1 = "Hard work always pays back" \n\t\tstring2 = Soon I will be Angular IT Champ`);
 function reverseString(str){
     var reverseStringResult="";
     for (let index = str.length-1; index >=0; index--) {
@@ -11,7 +11,8 @@ console.log(`\n Reverse given string using function and only charAt() = ${revers
 }
 reverseString("Hard work always pays back");
 reverseString("Soon I will be Angular IT Champ");
-
+console.log(`---------------------------------------------------------------------------------`);
+console.log(`Program for reverse first word from string : "Good Morning" `);
 var string1="Good Morning";
 var splitedString=string1.split("");
 var firstWord=splitedString.slice(0,4).join("");
@@ -23,3 +24,4 @@ for (let index = firstWord.length-1; index >=0; index--) {
 }
 console.log("\n string before reverse first word =",string1);
 console.log(`\nString after reverse first word = ${finalString}`);
+console.log(`\n`);
