9f4f392c94b4f565613bedd15cabbe34718dd4a9
UJadhav01 <111181029+UJadhav01@users.noreply.github.com>
2022-10-18 09:36:08 +0530

    while_loop.js

---
+++ jsFiles/06_while_loop.js
@@ -0,0 +1,15 @@
+var count = 10; // initialization
+while (count>=0) { // condition
+    console.log(count); 
+    count--; // update 
+
+}
+
+var table = 5;
+ var count = 1;
+ while(count <= 10)
+ {
+    var res = table * count;
+    console.log(res);
+    count++;
+ }
