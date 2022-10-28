d8f6448164604083d8e5f404d70a1db30bd110ca
UJadhav01 <111181029+UJadhav01@users.noreply.github.com>
2022-10-29 00:19:24 +0530

    array_basics

--- jsFiles/11_array_basics.js
+++ jsFiles/11_array_basics.js
@@ -3,13 +3,85 @@ console.log(arrayOfNumber);
 var arrayOfNumbers = [1, 3, 7, 8, 9, 3, 7];
 console.log(arrayOfNumbers);
  var arrayLength = arrayOfNumbers.length;
+ var mergedArray=arrayOfNumber.concat(arrayOfNumbers);
+ //remove duplicate elements from array
+/**A Set is a collection of unique values. To remove duplicates from an array:
 
-console.log(`Total number elements available in array: ${arrayLength}`);
+First, convert an array of duplicates to a Set. The new Set will implicitly remove duplicate elements.
+Then, convert the set back to an array.
+The following example uses a Set to remove duplicate */
+var arrayConvertedToSet=new Set(arrayOfNumber);
+//var uniqueArray=[...new Set(arrayOfNumber)]; instead of above u can write this way
+console.log(`uniq elements of arrayOfNumber array using set ${[...arrayConvertedToSet]}`);
+console.log(`uniq elements of arrayOfNumber array using set ${[...new Set(arrayOfNumbers)]}`);
+console.log(`-----------------------------------------------------------------------------------------------------------`);
 
-console.log(`Element at 4th Index is: ${arrayOfNumbers[4]}`);
+//The following example uses a indexof()to remove duplicate */
+let uniqrry=[];
+function getUniqueEle(data){
+    for (let index = 1; index < data.length; index++) {
+        if(uniqrry.indexOf(index)==-1){
+        uniqrry.push(index);}
+    }
+return uniqrry;
+}
+console.log(`uniq elements of arrayOfNumber array using indexof ${[...getUniqueEle(arrayOfNumber)]}`);
+console.log(`-----------------------------------------------------------------------------------------------------------`);
+   
+// WAP to find largest number from given array
+console.log(`-----------------------------------------------------------------------------------------------------------`);
 
-arrayOfNumbers[2] = 77;
-console.log(arrayOfNumbers);
+// WAP to find second largest number from given array
+console.log(`-----------------------------------------------------------------------------------------------------------`);
+// Addition of all elements in array using for loop
+function elementAdd(data){
+    var count=0;
+
+        for (let index = 0; index < data.length; index+=1) {
+                count+=data[index];
+        }
+    return count;
+    }
+    console.log(`addition of elements in array using simple for loop = ${elementAdd(arrayOfNumber)}`);
+
+    // 2nd way addition of array using for each
+    function sumArray(array) {
+        let sum = 0;
+       /*3rd way loop over array and add each item to sum
+  for (const item of array) {
+    sum += item;
+  } */
+        array.forEach(item => {
+          sum += item;
+        });
+      
+        console.log(`Addition of array using for each loop = `,sum);
+        return sum;
+      }
+      
+      sumArray([1, 4, 0, 9, -3]);
+
+console.log(`-----------------------------------------------------------------------------------------------------------`);
+
+// WAP to reverse given array
+let reversedArray=arrayOfNumber.reverse();
+console.log(`reversed array using reverse() = ${[...reversedArray]}`);
+console.log(`-----------------------------------------------------------------------------------------------------------`);
+
+// merge the two arrays
+console.log(`Array after merge using concat()\n arrayOfNumber with arrayOfNumbers = ${mergedArray}`);
+console.log(`-----------------------------------------------------------------------------------------------------------`);
+console.log(`Array after merge using ...spread operator\narrayOfNumber with arrayOfNumbers = ${[...arrayOfNumber,...arrayOfNumbers]}`);
+
+// console.log(`Total number elements available in array: ${arrayLength}`);
+
+// console.log(`Element at 4th Index is: ${arrayOfNumbers[4]}`);
+
+// arrayOfNumbers[2] = 77;
+// console.log(arrayOfNumbers);
+
+
+
 //console.log(typeof arrayOfNumbers);
 
 /**
