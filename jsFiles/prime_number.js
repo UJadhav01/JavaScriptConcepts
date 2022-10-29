ba851332746250adf2c5e26c06717637477a3b87
UJadhav01 <111181029+UJadhav01@users.noreply.github.com>
2022-10-29 16:02:16 +0530

    prime_number

--- jsFiles/prime_number.js
+++ jsFiles/prime_number.js
@@ -10,3 +10,18 @@ for(let i=2;i<Math.sqrt(num);i++){
         console.log("prime",i);
     }
 }
+
+//////
+function primeNumber(numberToCheck){
+    for (let index = 2; index < numberToCheck; index++) {
+      if(numberToCheck%index==0) {
+          return false;
+      }
+    }
+    return true;
+  }
+  
+  console.log(primeNumber(11));
+  console.log(primeNumber(9));
+  console.log(primeNumber(29));
+  
